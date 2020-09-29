56d909274cb22b2328b78e545c98900a0dfde1ea
Asmar <asmar.hajizada.std@bhos.edu.az>
2020-09-29 23:37:23 +0400

    added folder

---
+++ WEEK2/README.md
@@ -0,0 +1,5 @@
+## Week2
+Arrays, Pointers, Strings, 2D arrays
+
+#Assignment2
+A C program where I defined a function that takes two integers, swaps them, prints the swapped integers inside the function.
---
+++ WEEK2/assignment2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*Write a c program where you define a function that takes two integers,swaps them, prints the swapped 
+integers inside the function. You don't need to think in terms pointers, pointers will be taught in one to two weeks.
+Pass values by copy into the function.*/
+
+int swapIntegers(int int1, int int2);
+
+int main()
+{
+	int a, b;
+	a = 20;
+	b = 40;
+	printf("Given integers: a = %d; b = %d\n", a, b);
+	swapIntegers(a, b);
+
+	return 0;
+}
+
+int swapIntegers(int int1, int int2){
+
+	int temp;
+
+	temp = int1;
+	int1 = int2;
+	int2 = temp;
+
+	printf("Swapped integer values: a = %d; b = %d\n", int1, int2);
+	
+	return 0;	
+}
